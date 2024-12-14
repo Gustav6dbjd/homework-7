f10b5b50488f7f1ce482ae0e9f6563dd940c840a
Максим <141343669+Gustav6dbjd@users.noreply.github.com>
2024-12-14 19:47:33 +0300

    tasks

---
+++ script.js
@@ -0,0 +1,33 @@
+
+//           домашка на прошлый урок
+/*  
+let currentDate = new Date();
+console.log(currentDate.toDateString());
+
+let currentTime = new Date();
+console.log(currentTime.toLocaleTimeString());
+
+let currentDateTime = new Date();
+let formattedDateTime = currentDateTime.toISOString().slice(0, 19).replace('T', ' ');
+console.log(formattedDateTime);
+
+
+let startDate = new Date('2022-01-01');
+let today = new Date();
+let daysPassed = Math.floor((today - startDate) / (1000 * 60 * 60 * 24));
+console.log(daysPassed);
+
+
+let hoursPassed = Math.floor((today - startDate) / (1000 * 60 * 60));
+console.log(hoursPassed);
+
+
+let monthsPassed = (today.getFullYear() - startDate.getFullYear()) * 12 + (today.getMonth() - startDate.getMonth());
+console.log(monthsPassed);
+
+
+let yearsPassed = today.getFullYear() - startDate.getFullYear();
+console.log(yearsPassed);
+*/
+
+
